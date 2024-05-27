2507a7d97c068de1f4965b03315ed0c4fbcc8f7e
DeepikaVanka <vankadeepika1@gmail.com>
2024-05-27 08:23:57 +0530

    Added Example for the static and non-static

---
+++ Core_Java_Programs/src/basic_programs/Non_Static_Variable.java
@@ -0,0 +1,12 @@
+package basic_programs;
+
+public class Non_Static_Variable {
+     int i=100;
+     
+	public static void main(String[] args) {
+		// Example for the Non_static_Variables
+		Non_Static_Variable value = new Non_Static_Variable();
+		System.out.println(value.i);
+	}
+
+}
---
+++ Core_Java_Programs/src/basic_programs/Static_Variable.java
@@ -0,0 +1,16 @@
+package basic_programs;
+
+public class Static_Variable {
+	
+	static int i=100;
+
+	public static void main(String[] args) {
+		
+		// Static Variables
+		System.out.println("The Value is:: "+i);
+
+	}
+	
+	
+
+}
